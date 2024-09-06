384059315fadf0c47195549d72a40a0a9151247a
Yana <yanalytvynenko@gmail.com>
2024-09-06 17:55:25 +0200

    Update musics.repository.ts

--- backend/src/core/repositories/musics.repository.ts
+++ backend/src/core/repositories/musics.repository.ts
@@ -1028,13 +1028,12 @@ export class MusicsRepository
       // const maxPrice = allPrices.length > 0 ? Math.max(...allPrices) : 0;
       const maxPrice = cost;
       const minPrice = allPrices.length > 0 ? Math.min(...allPrices) : 0;
-      console.log('SSSSSS', rows);
-      const musics = rows.filter((music) =>
-        music.files.some((file) =>
-          file.cost >= cost.gte && file.cost <= cost.lte
-        )
-      );
-      // const musics = rows;
+      // const musics = rows.filter((music) =>
+      //   music.files.some((file) =>
+      //     file.cost >= cost.gte && file.cost <= cost.lte
+      //   )
+      // );
+      const musics = rows;
 
       return { musics, count, maxPrice, minPrice };
     } catch (error)
