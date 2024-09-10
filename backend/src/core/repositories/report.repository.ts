c2c906774cab30d3d9e4581a11de1d3bbf09fea1
Yana <yanalytvynenko@gmail.com>
2024-09-10 10:33:23 +0200

    Update report.repository.ts

--- backend/src/core/repositories/report.repository.ts
+++ backend/src/core/repositories/report.repository.ts
@@ -1,8 +1,9 @@
-import {
-  Inject,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import
+  {
+    Inject,
+    Injectable,
+    InternalServerErrorException,
+  } from '@nestjs/common';
 import { FindOptions } from 'sequelize';
 
 import { REPORTS_REPOSITORY } from '../constants';
@@ -10,51 +11,65 @@ import Reports from '../models/reports.entity';
 import { TReport } from '../types/reports';
 
 @Injectable()
-export class ReportRepository {
+export class ReportRepository
+{
   constructor(
     @Inject(REPORTS_REPOSITORY)
     private readonly reportRepository: typeof Reports,
-  ) {}
+  ) { }
 
-  async createReport(data: TReport): Promise<void> {
-    try {
+  async createReport(data: TReport): Promise<void>
+  {
+    try
+    {
+      console.log('WAW', data);
       await this.reportRepository.create(data);
-    } catch (error) {
+    } catch (error)
+    {
       throw new InternalServerErrorException(error.message);
     }
   }
 
-  async findAll(options: FindOptions): Promise<Reports[]> {
-    try {
+  async findAll(options: FindOptions): Promise<Reports[]>
+  {
+    try
+    {
       return await this.reportRepository.findAll({
         ...options,
         include: [{ all: true }],
       });
-    } catch (error) {
+    } catch (error)
+    {
       throw new InternalServerErrorException(error.message);
     }
   }
 
-  async findOne(id: number): Promise<Reports> {
-    try {
+  async findOne(id: number): Promise<Reports>
+  {
+    try
+    {
       return await this.reportRepository.findOne({
         where: { id },
         include: [{ all: true }],
       });
-    } catch (error) {
+    } catch (error)
+    {
       throw new InternalServerErrorException(error.message);
     }
   }
 
-  async update(id: number, data: Partial<TReport>): Promise<Reports> {
-    try {
+  async update(id: number, data: Partial<TReport>): Promise<Reports>
+  {
+    try
+    {
       const [, [updatedReport]] = await this.reportRepository.update(
         { ...data },
         { where: { id }, returning: true },
       );
 
       return updatedReport;
-    } catch (error) {
+    } catch (error)
+    {
       throw new InternalServerErrorException(error.message);
     }
   }
