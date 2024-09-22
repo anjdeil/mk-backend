0245a6c1875deae1b9c055c7d137def52455b215
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-09-22 22:12:29 +0200

    Update 202405281611540-update-types-enum.js

--- backend/src/database/migrations/202405281611540-update-types-enum.js
+++ backend/src/database/migrations/202405281611540-update-types-enum.js
@@ -1,8 +1,15 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(async (transaction) => {
+  up: async (queryInterface, Sequelize) =>
+  {
+    return queryInterface.sequelize.transaction(async (transaction) =>
+    {
+      await queryInterface.sequelize.query(
+        `ALTER TYPE "enum_notifications_type" ADD VALUE 'MUSIC_APPROVED';`,
+        { transaction },
+      );
+
       await queryInterface.sequelize.query(
         `ALTER TYPE "enum_notifications_type" ADD VALUE 'NEW_PLAYLISTS_FOLLOWER';`,
         { transaction },
@@ -10,8 +17,10 @@ module.exports = {
     });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(async (transaction) => {
+  down: async (queryInterface, Sequelize) =>
+  {
+    return queryInterface.sequelize.transaction(async (transaction) =>
+    {
       await queryInterface.sequelize.query(
         `ALTER TYPE "enum_notifications_type" DROP VALUE 'NEW_PLAYLISTS_FOLLOWER';`,
         { transaction },
