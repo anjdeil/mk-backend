ff4caddb3d6fb220a7a5f6879a8ac1b043e16c54
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-09-26 17:11:33 +0200

    Update reports.service.ts

--- backend/src/api/reports/reports.service.ts
+++ backend/src/api/reports/reports.service.ts
@@ -6,40 +6,54 @@ import { ReportRepository } from '../../core/repositories';
 import { TReport } from '../../core/types';
 
 @Injectable()
-export class ReportsService {
-  constructor(private readonly reportRepository: ReportRepository) {}
+export class ReportsService
+{
+  constructor(private readonly reportRepository: ReportRepository) { }
 
   async createReport(
     data: Omit<TReport, 'userId'>,
     userId: number,
-  ): Promise<void> {
-    try {
+  ): Promise<void>
+  {
+    try
+    {
       return await this.reportRepository.createReport({ ...data, userId });
-    } catch (error) {
+    } catch (error)
+    {
       throw new InternalServerErrorException(error.message);
     }
   }
 
-  async findAll(options: FindOptions): Promise<Reports[]> {
-    try {
+  async findAll(options: FindOptions): Promise<Reports[]>
+  {
+    try
+    {
       return await this.reportRepository.findAll(options);
-    } catch (error) {
+    } catch (error)
+    {
+      console.log('errorororoor:', error.message);
       throw new InternalServerErrorException(error.message);
     }
   }
 
-  async findOne(id: number): Promise<Reports> {
-    try {
+  async findOne(id: number): Promise<Reports>
+  {
+    try
+    {
       return await this.reportRepository.findOne(id);
-    } catch (error) {
+    } catch (error)
+    {
       throw new InternalServerErrorException(error.message);
     }
   }
 
-  async update(id: number, data: Partial<TReport>): Promise<Reports> {
-    try {
+  async update(id: number, data: Partial<TReport>): Promise<Reports>
+  {
+    try
+    {
       return await this.reportRepository.update(id, data);
-    } catch (error) {
+    } catch (error)
+    {
       throw new InternalServerErrorException(error.message);
     }
   }
