237dd919066b83fd3cd68b681fb2f32df8de3042
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-10-01 10:17:59 +0200

    Update stripe.service.ts

--- backend/src/api/stripe/stripe.service.ts
+++ backend/src/api/stripe/stripe.service.ts
@@ -229,7 +229,7 @@ export class StripeService
               subscribedUntil,
               subscriptionPriceId: data.items.data[0].price.id,
             });
-            console.log('AfterFirstOne :>>', data);
+            console.log('AfterFirstOne :>>', data.metadata);
             try
             {
               await this.musicsRepository.unblockMusicsByUserId(
