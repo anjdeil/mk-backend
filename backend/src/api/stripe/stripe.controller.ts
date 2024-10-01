4a614300832e646c5f6603bf469e424de512301c
IliaDigiWay <168741673+IliaDigiWay@users.noreply.github.com>
2024-10-01 11:14:40 +0200

    Update stripe.controller.ts

--- backend/src/api/stripe/stripe.controller.ts
+++ backend/src/api/stripe/stripe.controller.ts
@@ -1,20 +1,22 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Headers,
-  Post,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
+import
+  {
+    BadRequestException,
+    Body,
+    Controller,
+    Headers,
+    Post,
+    Req,
+    UseGuards,
+  } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import {
-  ApiBearerAuth,
-  ApiBody,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import
+  {
+    ApiBearerAuth,
+    ApiBody,
+    ApiOperation,
+    ApiResponse,
+    ApiTags,
+  } from '@nestjs/swagger';
 import Stripe from 'stripe';
 
 import { StripeService } from './stripe.service';
@@ -25,8 +27,9 @@ import { AuthRequest } from '../../core/types/common';
 
 @ApiTags('stripe')
 @Controller('stripe')
-export class StripeController {
-  constructor(private readonly stripeService: StripeService) {}
+export class StripeController
+{
+  constructor(private readonly stripeService: StripeService) { }
 
   @ApiBearerAuth()
   @ApiOperation({ summary: 'Create customer' })
@@ -34,7 +37,8 @@ export class StripeController {
   @AllowedRoles(Roles.SELLER, Roles.BUYER)
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Post('create-customer')
-  public async createCustomer(@Req() req: AuthRequest) {
+  public async createCustomer(@Req() req: AuthRequest)
+  {
     return await this.stripeService.createCustomer(req.user);
   }
 
@@ -57,7 +61,8 @@ export class StripeController {
   })
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Post('payment-confirm')
-  public async confirmPayment(@Req() req: AuthRequest) {
+  public async confirmPayment(@Req() req: AuthRequest)
+  {
     return await this.stripeService.confirmPayment(
       req.body.paymentIntent,
       req.body.paymentMethodId,
@@ -87,7 +92,8 @@ export class StripeController {
   @AllowedRoles(Roles.SELLER, Roles.BUYER)
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Post('create-payment-intent')
-  public async createPaymentIntent(@Req() req: AuthRequest, @Body() data) {
+  public async createPaymentIntent(@Req() req: AuthRequest, @Body() data)
+  {
     return await this.stripeService.createPaymentIntent(
       data.amount,
       req.user,
@@ -99,10 +105,14 @@ export class StripeController {
   async handleIncomingEvents(
     @Headers('stripe-signature') signature: string,
     @Body() body: Stripe.Event,
-  ) {
-    if (!signature) {
+    @Req() request: AuthRequest,
+  )
+  {
+    if (!signature)
+    {
       throw new BadRequestException('Missing stripe-signature header');
     }
+    console.log('Webhook event received', request.user.id);
     await this.stripeService.handleEvent(body);
     return true;
   }
